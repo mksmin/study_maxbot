204b0a444205ae85978f2014d761683a07fa4fdb
mks_min <mininname@vk.com>
2026-03-13 01:00:10 +0300

    chore: refactor main.go to use new dispatcher and routers

--- main.go
+++ main.go
@@ -2,9 +2,6 @@ package main
 
 import (
 	"context"
-	"encoding/json"
-	"fmt"
-	"log"
 	"os"
 	"os/signal"
 	"syscall"
@@ -12,68 +9,62 @@ import (
 	"github.com/joho/godotenv"
 	maxbot "github.com/max-messenger/max-bot-api-client-go"
 	"github.com/max-messenger/max-bot-api-client-go/schemes"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+
+	dp "study_bot_go/dispatcher"
+	"study_bot_go/handlers"
 )
 
 func main() {
+	log.Logger = log.Output(
+		zerolog.ConsoleWriter{
+			Out:        os.Stderr,
+			TimeFormat: "15:04:05"},
+	)
+
 	ctx, stop := signal.NotifyContext(
 		context.Background(),
 		syscall.SIGTERM,
 		os.Interrupt,
 	)
 	defer stop()
-	defer fmt.Println("Stopping app")
 
 	if err := godotenv.Load(".env"); err != nil {
-		log.Printf("Warning: could not load .env file: %v", err)
+		log.Warn().Msg("Could not load .env file")
 	}
 
-	api, _ := maxbot.New(os.Getenv("MAX_BOT_TOKEN"))
+	api, err := maxbot.New(
+		os.Getenv("MAX_BOT_TOKEN"),
+	)
+	if err != nil {
+		log.Fatal().Err(err).Msg("API initialization failed")
+	}
 
-	info, err := api.Bots.GetBot(ctx)
-	bot_info, _ := json.MarshalIndent(info, "", " ")
-	log.Println("Bot info:")
-	log.Println(string(bot_info))
-	log.Printf("Err %v", err)
+	dispatcher := dp.NewDispatcher()
+	dispatcher.IncludeRouter(handlers.StartRouter())
+	dispatcher.IncludeRouter(handlers.CommandsRouter())
+
+	log.Info().Msg("Бот успешно запущен!")
 
 	for upd := range api.GetUpdates(ctx) {
 		switch u := upd.(type) {
 		case *schemes.BotStartedUpdate:
-			message := maxbot.NewMessage().
+			log.Info().
+				Int64(
+					"chat_id",
+					u.ChatId,
+				).
+				Msg("Bot started in chat")
+			msg := maxbot.NewMessage().
 				SetChat(u.ChatId).
-				SetText("Добро пожаловать в бота")
+				SetText("Добро пожаловать!")
+			_ = api.Messages.Send(ctx, msg)
 
-			fmt.Printf("User %v from chat %v ", u.User, u.ChatId)
-
-			err := api.Messages.Send(ctx, message)
-			if err != nil {
-			}
 		case *schemes.MessageCreatedUpdate:
-			out := "bot прочитал текст: " + u.GetText()
-
-			switch u.GetCommand() {
-			case "/start":
-				out = "Команда: " + u.GetCommand()
-				message := maxbot.NewMessage().
-					SetUser(u.Message.Sender.UserId).
-					SetText(out)
-				err := api.Messages.Send(ctx, message)
-				if err != nil {
-				}
-			default:
-				userMessage := u.Message
-				b, _ := json.MarshalIndent(userMessage, "", "  ")
-				fmt.Println()
-				fmt.Println(fmt.Sprintf("Get message: %v", string(b)))
-
-				message := maxbot.NewMessage().
-					SetUser(userMessage.Sender.UserId).
-					Reply("Hello from bot", userMessage)
-
-				err := api.Messages.Send(ctx, message)
-				if err != nil {
-				}
+			if err := dispatcher.Handle(ctx, api, u); err != nil {
+				log.Error().Err(err).Msg("Ошибка в обработчике")
 			}
-
 		}
 	}
 }
