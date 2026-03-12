d4aff2f1ed61d8f9a59b94c96669ebc2dcfabf7b
mks_min <mininname@vk.com>
2026-03-13 01:27:44 +0300

    refactor: use bot package types in dispatcher handler record

--- dispatcher/types.go
+++ dispatcher/types.go
@@ -1,23 +1,10 @@
 package dispatcher
 
 import (
-	"context"
-
-	maxbot "github.com/max-messenger/max-bot-api-client-go"
-	"github.com/max-messenger/max-bot-api-client-go/schemes"
+	"study_bot_go/internal/bot"
 )
 
-type Handler func(
-	ctx context.Context,
-	api *maxbot.Api,
-	u *schemes.MessageCreatedUpdate,
-) error
-
-type Filter func(
-	u *schemes.MessageCreatedUpdate,
-) bool
-
 type handlerRecord struct {
-	handler Handler
-	filters []Filter
+	handler bot.Handler
+	filters []bot.Filter
 }
