e2c7bc1a838ee2f1508b3b837b917e3da2f92524
mks_min <mininname@vk.com>
2026-03-13 01:27:32 +0300

    feat: add base bot types for handler and filter

---
+++ internal/bot/types.go
@@ -0,0 +1,18 @@
+package bot
+
+import (
+	"context"
+
+	maxbot "github.com/max-messenger/max-bot-api-client-go"
+	"github.com/max-messenger/max-bot-api-client-go/schemes"
+)
+
+type Handler func(
+	ctx context.Context,
+	api *maxbot.Api,
+	u *schemes.MessageCreatedUpdate,
+) error
+
+type Filter func(
+	u *schemes.MessageCreatedUpdate,
+) bool
